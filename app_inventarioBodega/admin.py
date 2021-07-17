c53b55900746a8e1a91f86cd7f3be0f956d53433
Melva-99 <melvarobertina.83@outlook.com>
2021-07-17 00:05:10 -0600

    Modelos

--- app_inventarioBodega/admin.py
+++ app_inventarioBodega/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Empleado,Bodega,Producto
 # Register your models here.
+
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'apellido', 'correo','fechaIngreso')
+
+class BodegaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'encargado',)
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'categoria','stock','cantidad','precio','bodega')
+
+admin.site.register(Empleado,EmpleadoAdmin)
+
+admin.site.register(Bodega,BodegaAdmin)
+
+admin.site.register(Producto,ProductoAdmin)
--- app_inventarioBodega/models.py
+++ app_inventarioBodega/models.py
@@ -1,3 +1,81 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
+class Empleado(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    direccion = models.TextField()
+    telefono = models.IntegerField()
+    correo = models.EmailField()
+    fechaIngreso = models.DateField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Bodega(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    direccion = models.TextField()
+    capacidad = models.IntegerField()
+    encargado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    CATEGORIAS = (
+        ('1', 'Lácteos'),
+        ('2', 'Carnes'),
+        ('3', 'Bebidas'),
+        ('4', 'Limpieza'),
+        ('5', 'Abarrotes'),
+        ('6', 'Enlatados'),
+        ('7', 'Granos'),
+        ('8', 'Dulces'),
+        ('9', 'Otros'),
+    )
+    MEDIDAS = (
+        ('1', 'Libras'),
+        ('2', 'Kilos'),
+        ('3', 'Cajas'),
+        ('4', 'Docena'),
+        ('5', 'Media docena'),
+        ('6', 'Quintal'),
+        ('7', 'Litros'),
+    )
+    nombre = models.CharField(max_length=30)
+    categoria = models.CharField(max_length=1, choices=CATEGORIAS, default='1')
+    unidades_medidas = models.CharField(max_length=1, choices=MEDIDAS, default='1')
+    stock = models.IntegerField() 
+    cantidad = models.IntegerField()
+    precio = models.FloatField()
+    bodega = models.ForeignKey(Bodega, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+'''class Transferencia(models.Model):
+
+    TIPO = (
+        ('1', 'Transferencia'),
+        ('2', 'Orden de venta'),
+        ('3', 'Orden de compra'),
+    )
+
+    tipoTransferencia = models.CharField(max_length=1, choices=TIPO, default='1')
+    ordenCompra = models.IntegerField(null=True, blank=True)
+    precioCompra = models.FloatField(null=True, blank=True)
+    precioVenta = models.FloatField(null=True, blank=True)
+    precioTransferencia = models.FloatField()
+    bodegaOrigen = models.ForeignKey(Bodega,related_name="bodegaOrigen" ,on_delete=models.CASCADE)
+    bodegaDestino = models.ForeignKey(Bodega,related_name="bodegaDestino" ,on_delete=models.CASCADE, null=True, blank=True)
+    fecha = models.DateField()
+
+    def __str__(self):
+        return f'{self.tipoTransferencia}'''
