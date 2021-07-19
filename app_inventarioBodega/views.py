d05837442699545e5a7dec36d1a9f85a09ccc09c
Nicole Aguilar <nicole_a51@outlook.es>
2021-07-18 21:39:33 -0600

    View, Url de Empleados

--- app_inventarioBodega/views.py
+++ app_inventarioBodega/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Empleado
 
+#Vistas
 
+def index(request):
+    return render(request, 'bodega/index.html')
+
+def empleados(request):
+    if request.method == "POST":
+        #informacion de empleados
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        direccion = request.POST.get('direccion')
+        telefono = request.POST.get('telefono')
+        fechaIngreso = request.POST.get('fechaIgreso')
+
+#em = empleados
+        em = Empleado(nombre=nombre, apellido=apellido, direccion=direccion, telefono=telefono, fechaIngreso=fechaIngreso)
+        em.save() #insert a la base de datos
+
+        msj = f'El Empleado {nombre} {apellido} ha sido registrado.'
+
+        data = Empleado.objects.all()
+
+    ''' ctx = {
+        'empleados': data,
+        
+    }'''
+
+    return render(request, 'bodega/empleado.html')
