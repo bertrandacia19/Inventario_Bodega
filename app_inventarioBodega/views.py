43658a1be80fe317de1c8b0f4760ddce6af816d0
Nicole Aguilar <nicole_a51@outlook.es>
2021-07-19 14:44:04 -0600

    Arreglo Errores View, Empleados

--- app_inventarioBodega/views.py
+++ app_inventarioBodega/views.py
@@ -13,19 +13,14 @@ def empleados(request):
         apellido = request.POST.get('apellido')
         direccion = request.POST.get('direccion')
         telefono = request.POST.get('telefono')
-        fechaIngreso = request.POST.get('fechaIgreso')
+        correo = request.POST.get('correo')
+        fechaIngreso = request.POST.get('fechaIngreso')
 
 #em = empleados
-        em = Empleado(nombre=nombre, apellido=apellido, direccion=direccion, telefono=telefono, fechaIngreso=fechaIngreso)
+        em = Empleado(nombre=nombre, apellido=apellido, direccion=direccion, telefono=telefono, correo=correo, fechaIngreso=fechaIngreso)
         em.save() #insert a la base de datos
 
         msj = f'El Empleado {nombre} {apellido} ha sido registrado.'
 
-        data = Empleado.objects.all()
-
-    ''' ctx = {
-        'empleados': data,
-        
-    }'''
-
-    return render(request, 'bodega/empleado.html')
+    empleados = Empleado.objects.all().order_by('fechaIngreso')
+    return render(request, 'bodega/empleado.html', {'empleados':empleados})
