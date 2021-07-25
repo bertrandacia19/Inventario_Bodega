75df01b3004856a2d64ca0c8456017b3d870e202
akumanam <nahumlop17@gmail.com>
2021-07-25 13:00:36 -0600

    Update urls.py

--- app_inventarioBodega/urls.py
+++ app_inventarioBodega/urls.py
@@ -6,5 +6,6 @@ app_name = 'inventario'
 urlpatterns = [
     path('', views.index, name='index'),
     path('bodega/', views.bodega, name='bodega'),
+    path('empleado/', views.empleados, name="empleado"),
 
 ]
