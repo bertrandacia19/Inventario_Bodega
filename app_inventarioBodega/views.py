2ca5fce72df449f155f558a292f817b2579d544d
Kevin Valladares <kevinvduarte317@gmail.com>
2021-07-25 22:02:20 -0600

    Ajustes en views y plantillas

--- app_inventarioBodega/views.py
+++ app_inventarioBodega/views.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from .models import Producto, Bodega
+from django.contrib import messages
 
 def nuevoProducto(request):
     if request.method == 'POST':
@@ -15,15 +16,35 @@ def nuevoProducto(request):
         bodega = Bodega.objects.get(pk=Pbodega)
         
         if Producto.objects.filter(nombre__contains=nombre):
-            return JsonResponse({'color':'error','msj':'El producto ya existe.'})
+
+            data = Producto.objects.all()
+            q = request.GET.get('q')
+            ctx = {
+                'productos': data,
+                'q': q,
+                'bodega': Bodega.objects.all(),
+                'msj':'danger',
+            }
+            msj = 'El producto ya existe!'
+            messages.add_message(request,messages.INFO, msj)
+            return render(request, 'bodega/nuevoProducto.html', ctx)
 
         p = Producto(nombre=nombre, categoria=categoria, unidades_medidas=unidades_medidas, cantidad=cantidad, proveedor=proveedor, precio_compra=precio_compra, precio_venta=precio_venta,bodega=bodega)
         p.save()
-
-        return JsonResponse({'color':'success','msj':'Se a guardado el producto exitosamente.'})
+        msj = 'El producto ha sido registrado correctamente.'
+        messages.add_message(request,messages.INFO, msj)
+        
+        data = Producto.objects.all()
+        q = request.GET.get('q')
+        ctx = {
+            'productos': data,
+            'q': q,
+            'bodega': Bodega.objects.all(),
+            'msj':'success',
+        }
+        return render(request, 'bodega/nuevoProducto.html', ctx)
     
     q = request.GET.get('q')
-
     if q:
         productos = Producto.objects.filter(nombre__startswith=q).order_by('nombre')
     else:
@@ -60,7 +81,14 @@ def actualizarProducto(request, id):
         add_producto.cantidad += add_cantidad
         add_producto.save()
         
-        return JsonResponse({'color':'success','msj':'Se a agregado al producto existosamente.'})
+        msj = 'El producto ha sido actualizado correctamente.'
+        messages.add_message(request,messages.INFO, msj)
+        
+        productos = Producto.objects.all()
+        ctx ={
+            'productos': productos,
+        }
+        return render(request, 'bodega/cantidadProducto.html', ctx)
 
     data = Producto.objects.all()
 
